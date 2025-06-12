95483b321d05dcf543673ea763e93d02ba4cf806
DiiNguyen24 <duy242004@gmail.com>
2025-06-13 01:48:20 +0700

    API get post ID Technical

---
+++ SWP391-BDNAT-API/BDNAT-Service/Interface/ITechnicalService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BDNAT_Repository.DTO;
+
+namespace BDNAT_Service.Interface
+{
+    public interface ITechnicalService
+    {
+        Task<List<TechnicalDTO>> GetAllTechnicalAsync();
+        Task<TechnicalDTO> GetTechnicalByIdAsync(int id);
+        Task<bool> CreateTechnicalAsync(TechnicalDTO Technical);
+        Task<bool> UpdateTechnicalAsync(TechnicalDTO Technical);
+        Task<bool> DeleteTechnicalAsync(int id);
+    }
+}
--- SWP391-BDNAT-API/SWP391-BDNAT-API/Controllers/TechnicalController.cs
+++ SWP391-BDNAT-API/SWP391-BDNAT-API/Controllers/TechnicalController.cs
@@ -1,54 +1,69 @@
-﻿//using System;
-//using BDNAT_Repository.Entities;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
+﻿using System;
+using BDNAT_Repository.DTO;
+using BDNAT_Repository.Entities;
+using BDNAT_Service.Implementation;
+using BDNAT_Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-//namespace SWP391_BDNAT_API.Controllers
-//{
+namespace SWP391_BDNAT_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TechnicalController : Controller
 
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class TechnicalServiceController : ControllerBase
-//    {
-//        private readonly AppDbContext _context;
-//        public TechnicalServiceController(AppDbContext context) => _context = context;
+    {
+        private readonly ITechnicalService _TechnicalService;
 
-//        [HttpGet] // Lấy toàn bộ technical service
-//        public async Task<ActionResult<IEnumerable<TechnicalService>>> GetAll() => await _context.TechnicalServices.ToListAsync();
+        public TechnicalController(ITechnicalService TechnicalService)
+        {
+            _TechnicalService = TechnicalService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<BlogDTO>>> GetAllTechnicalAsync()
+        {
+            try
+            {
+                //var blogs = await _blogService.GetAllBlogsAsync();
+                var Technicals = await _TechnicalService.GetAllTechnicalAsync();
+                return Ok(Technicals);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult<bool>> CreateTechnical([FromBody] TechnicalDTO TechnicalDto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
 
-//        [HttpGet("{id}")] // Lấy theo ID
-//        public async Task<ActionResult<TechnicalService>> GetById(int id)
-//        {
-//            var entity = await _context.TechnicalServices.FindAsync(id);
-//            return entity == null ? NotFound() : Ok(entity);
-//        }
+                var result = await _TechnicalService.CreateTechnicalAsync(TechnicalDto);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TechnicalDTO>> GetTechnicalById(int id)
+        {
+            try
+            {
+                var Technical = await _TechnicalService.GetTechnicalByIdAsync(id);
+                if (Technical == null)
+                    return NotFound($"Blog with ID {id} not found");
+                return Ok(Technical);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
 
-//        [HttpPost] // Tạo mới
-//        public async Task<ActionResult<TechnicalService>> Create(TechnicalService item)
-//        {
-//            _context.TechnicalServices.Add(item);
-//            await _context.SaveChangesAsync();
-//            return CreatedAtAction(nameof(GetById), new { id = item.TechServiceID }, item);
-//        }
-
-//        [HttpPut("{id}")] // Cập nhật
-//        public async Task<IActionResult> Update(int id, TechnicalService item)
-//        {
-//            if (id != item.TechServiceID) return BadRequest();
-//            _context.Entry(item).State = EntityState.Modified;
-//            await _context.SaveChangesAsync();
-//            return NoContent();
-//        }
-
-//        [HttpDelete("{id}")] // Xoá
-//        public async Task<IActionResult> Delete(int id)
-//        {
-//            var entity = await _context.TechnicalServices.FindAsync(id);
-//            if (entity == null) return NotFound();
-//            _context.TechnicalServices.Remove(entity);
-//            await _context.SaveChangesAsync();
-//            return NoContent();
-//        }
-//    }
-//}
+    }
+}
